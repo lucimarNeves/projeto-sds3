62ecab829b06f9c70d7e3d6d88c4508f7a149fba
lucimarNeves <lucimar.neves@gmail.com>
2021-05-07 23:15:45 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,5 +1,12 @@
+import { type } from "node:os"
+
 export type SaleSum =
     {
-        sellerName: string,
-        sum: number
+        sellerName: string;
+        sum: number;
     }
+export type SalesSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
