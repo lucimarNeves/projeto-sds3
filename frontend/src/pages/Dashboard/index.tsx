c5074998dc86ca3b8619144b3ea35f1e92bf1c0f
lucimarNeves <lucimar.neves@gmail.com>
2021-05-10 21:05:23 -0300

    Alterando layout

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,22 +9,24 @@ const Dashboard = () => {
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary py-3"> DashBoard de Vendas</h1>
+                <div className="container2">
+                    <h1 className="text-primary py-3"> DashBoard de Vendas</h1>
 
-                <div className="row px-3">
-                    <div className="col-sm-6">
-                        <h5 className="text-center text-secondary"> Taxa de sucesso (%)</h5>
-                        <BarChart />
+                    <div className="row px-3">
+                        <div className="col-sm-6">
+                            <h5 className="text-center text-secondary"> Taxa de sucesso (%)</h5>
+                            <BarChart />
+                        </div>
+                        <div className="col-sm-6">
+                            <h5 className="text-center text-secondary"> Todas as vendas</h5>
+                            <DonutChart />
+                        </div>
                     </div>
-                    <div className="col-sm-6">
-                        <h5 className="text-center text-secondary"> Todas as vendas</h5>
-                        <DonutChart />
+                    <div className="py-3">
+                        <h2 className="text-primary"> Todas as vendas</h2>
                     </div>
+                    <DataTable />
                 </div>
-                <div className="py-3">
-                    <h2 className="text-primary"> Todas as vendas</h2>
-                </div>
-                <DataTable />
             </div>
             <Footer />
         </>
